730ca752864ee372aae0b5a0690262aaa434a202
JWL <66761864+bovob@users.noreply.github.com>
2024-08-20 23:27:55 +0900

    [Bronze V] Title: 개, Time: 96 ms, Memory: 13944 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,16 @@
+
+public class Main {
+    public static void main (String[] args) {
+        String[] str = {
+        "|\\_/|",
+        "|q p|   /}",
+        "( 0 )\"\"\"\\",
+        "|\"^\"`    |",
+        "||_/=\\\\__|"
+        };
+
+        for(int i = 0 ; i < str.length ; i++){
+            System.out.println(str[i]);
+        }
+    }
+}
