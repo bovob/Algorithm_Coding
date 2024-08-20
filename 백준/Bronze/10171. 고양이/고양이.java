5fb92102adcee34d681fb491fde91f5ad8674919
JWL <66761864+bovob@users.noreply.github.com>
2024-08-20 23:25:34 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 13896 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main (String[] args) {
+        String[] str = {
+            "\\    /\\",
+            " )  ( ')",
+            "(  /  )",
+            " \\(__)|"
+        };
+
+        for(int i = 0 ; i < str.length ; i++){
+            System.out.println(str[i]);
+        }
+    }
+}
