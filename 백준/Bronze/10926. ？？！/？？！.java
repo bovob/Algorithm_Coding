6c2b062548dd9226cfb8edb1d75c5a05aada2a86
JWL <66761864+bovob@users.noreply.github.com>
2024-08-20 22:57:20 +0900

    [Bronze V] Title: ??!, Time: 172 ms, Memory: 17772 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String id = scan.nextLine();
+        System.out.printf("%s??!",id);
+
+
+        scan.close();
+    }
+}
