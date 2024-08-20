bd401f833dfbc3213b15ffdd43dfec90fda081ae
JWL <66761864+bovob@users.noreply.github.com>
2024-08-20 22:51:44 +0900

    [Bronze V] Title: A×B, Time: 172 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+
+        System.out.printf("%d", a*b);
+        scan.close();
+    }
+}
+
+
